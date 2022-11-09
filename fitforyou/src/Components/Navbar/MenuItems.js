0dcbf4bcfa21c002e76c75678a0bac21bf5066aa
JordanMcFadden <jordan.d.mcfadden-1@ou.edu>
2022-11-09 11:21:34 -0600

    Project push to Github

---
+++ fitforyou/src/Components/Navbar/MenuItems.js
@@ -0,0 +1,24 @@
+export const MenuItems = [
+
+    {
+        title: 'About FitForYou',
+        url: 'About',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'My Account',
+        url: 'MyAccount',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'Log In',
+        url: 'LogIn',
+        cName: 'nav-links'
+    },
+
+    
+    
+
+]
